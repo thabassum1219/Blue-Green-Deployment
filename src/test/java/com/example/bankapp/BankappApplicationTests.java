a8b1a6822fea76ff770c94ffe02b0428f214bd5c
Rajesh2b <imrajesh027@gmail.com>
2025-01-25 19:36:23 +0530

    Update BankappApplicationTests.java

--- src/test/java/com/example/bankapp/BankappApplicationTests.java
+++ src/test/java/com/example/bankapp/BankappApplicationTests.java
@@ -2,10 +2,9 @@ package com.example.bankapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+
 class BankappApplicationTests {
 
 	@Test
